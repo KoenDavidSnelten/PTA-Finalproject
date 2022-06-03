a12f83bf11e500593d1364be02ceecb92eae3dd3
gamemeloentje <koensnelten@gmail.com>
2022-06-03 17:14:01 +0200

    Made a start on flask website.

---
+++ server/__init__.py
@@ -0,0 +1,33 @@
+import os
+
+from flask import Flask
+from flask import render_template
+
+
+def create_app():
+    """
+    Create and configure an instance of the Flask application.
+    """
+
+    app = Flask(__name__)
+
+    # Makes sure that all application folders are created.
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def index():
+        """
+        Index page for the website.
+        route: `/`
+        This page shows some basis information about the
+        website and allows users to navigate to their desired pages.
+        """
+        return render_template('index.html')
+
+    from server.wikifier.routes import bp as assistant_bp
+    app.register_blueprint(assistant_bp)
+
+    return app
