1d60b118b76df7dc83b4857b6ecaf543a7c1a63d
gamemeloentje <koensnelten@gmail.com>
2022-06-07 13:37:15 +0200

    Worked on the flask server

--- server/wikifier/routes.py
+++ server/wikifier/routes.py
@@ -2,16 +2,44 @@ from flask import Blueprint
 from flask import render_template
 from flask import request
 
+#from server.core import long_sent_finder
+
 bp = Blueprint('wikifier', __name__, url_prefix='/wikifier')
 
-@bp.route('/')
+
+@bp.route('/', methods=['POST', 'GET'])
 def index():
     """
-    Index page for the assistant app
+    Index page for the wikifier app
     route: `/wikifier/`
-    This page shows the main assistant application.
+    This page shows the main wikifier application.
     """
+    if request.method == 'POST':
+        text = request.form['text'].strip()
+        return render_template(
+            'wikifier/index.html',
+            form_data={
+                'text': text,
+            },
+        )
 
     return render_template(
-        'assistant/index.html',
+        'wikifier/index.html',
+    )
+
+
+@bp.route('/wikify', methods=['POST'])
+def wikify():
+
+    text = request.form['text']
+
+    if len(text) == 0:
+        return render_template(
+            'wikifier/wikify.html',
+            error='Voer een geldige tekst in!',
+        )
+
+    return render_template(
+        'wikifier/index.html',
+        text=text,
     )
