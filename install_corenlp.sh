f26e239fc55282d82c9c47b1fe697c422cb4d705
Twan H <huiskenstwan@gmail.com>
2022-06-07 22:57:48 +0200

    Remove unnessesary script

--- install_corenlp.sh
+++
@@ -1,4 +0,0 @@
-wget http://nlp.stanford.edu/software/stanford-corenlp-full-2018-02-27.zip
-unzip stanford-corenlp-full-2018-02-27.zip
-cd stanford-corenlp-full-2018-02-27
-mv stanford-corenlp-full-2018-02-27 corenlp
